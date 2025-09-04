86f0838fdf6f22cd0eee01e2def66c0f98798c8d
emrearslandogan <earslndgn@gmail.com>
2025-09-05 02:59:15 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "stb_image.h" // the main library for us to read-write image
+
+using namespace std;
+
+class ToAscii {
+    private:
+        char palette[][] = ["@%#*+=-:. ", ""]; // TODO, have new palettes
+
+
+    public:
+        
+        // some helper functions
+        // helper: to grayscale
+        // helper: to characterize, according to specific resolution for the ascii image
+        // final: symbolizer
+        // extra: batch processing for multiple image files
+        // extra2: video processing, recorded video
+        // extra3: video processing, live
+        // extra4: multithreading or gpu usage? maybe?
+
+}
+
+
+int main(){
+
+
+    return 0;
+}
